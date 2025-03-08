ecefb8ec7d0673845eedd72f0173deff36c600e9
unknown <santoyoisabella14@gmail.com>
2025-03-08 17:03:10 -0400

    primer commit

--- ricky_martin/src/views/HomeView.vue
+++ ricky_martin/src/views/HomeView.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+
+const characters = ref(['Rick', 'Morty', 'Summer', 'Beth', 'Jerry'])
+
+
+
+
 
 </script>
 
 <template>
+    <div v-for="character in characters" :key="character.id">
+        <h2>{{ character }}</h2>
+
+
+
+    </div>
 
 </template>
 
