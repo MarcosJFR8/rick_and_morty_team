f23f38cb59add7c9bacae18b8351c6d39885db70
Daniel <damm172004@gmail.com>
2025-03-08 17:23:22 -0400

    paginacion added

--- ricky_martin/src/views/HomeView.vue
+++ ricky_martin/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { ref, onMounted, } from "vue";
 import { useRouter } from "vue-router";
+import { Character } from "../types/Character";
     
 const router = useRouter();
 const characters = ref<Character[]>([])
+const page:any = ref<number>(1)
 
 
     const findCharacters = () => {
@@ -11,7 +13,7 @@ const characters = ref<Character[]>([])
     };
 
     const loadCharacters = async () => {
-        const response = await fetch(`https://rickandmortyapi.com/api/character/`)
+        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page.value}`)
         const data = await response.json()
         characters.value = data.results
         console.log();
@@ -19,7 +21,21 @@ const characters = ref<Character[]>([])
 
     const seeCharacterDetails =(character_id: number)=>{
         router.push(`details/${character_id}`)
-    };   
+    };
+    
+    const incrementPage = () => {
+        if(page.value !== null){
+            page.value++
+            loadCharacters()
+        }
+    }
+
+    const decrementePage = () => {
+        if(page.value > 1){
+        page.value--
+        loadCharacters()
+        }
+    }
 
     onMounted(() => {
         loadCharacters()
@@ -27,6 +43,12 @@ const characters = ref<Character[]>([])
 </script>
 
 <template>
+
+    <div>
+        <button @click="incrementPage">+</button>
+        {{ page }}
+        <button @click="decrementePage">-</button>
+    </div>
 
 </template>
 
