cd2da3ccd4ceca93f6be572e9389dbc4eed0cb33
Marcos <marcosjfr3@gmail.com>
2025-03-08 18:33:25 -0400

    details view added

--- ricky_martin/src/views/HomeView.vue
+++ ricky_martin/src/views/HomeView.vue
@@ -1,44 +1,44 @@
 <script setup lang="ts">
 import { ref, onMounted, } from "vue";
 import { useRouter } from "vue-router";
-import  type { Character } from "../../types/character";
+import type { Character } from "@/types/character";
     
 const router = useRouter();
 const characters = ref<Character[]>([])
 const page:any = ref<number>(1)
 
 
-    const findCharacters = () => {
-        loadCharacters()
+const findCharacters = () => {
+    loadCharacters()
     };
 
-    const loadCharacters = async () => {
-        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page.value}`)
-        const data = await response.json()
-        characters.value = data.results
-        console.log();
+const loadCharacters = async () => {
+    const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page.value}`)
+    const data = await response.json()
+    characters.value = data.results
+    console.log();
     };
 
-    const seeCharacterDetails =(character_id: number)=>{
-        router.push(`/details/${character_id}`)
+const seeCharacterDetails =(character_id: number)=>{
+    router.push(`/details/${character_id}`)
     };   
 
-    const incrementPage = () => {
-        if(page.value !== null){
-            page.value++
-            loadCharacters()
-        }
-    }
-
-    const decrementePage = () => {
-        if(page.value > 1){
-        page.value--
+const incrementPage = () => {
+    if(page.value !== null){
+        page.value++
         loadCharacters()
         }
     }
 
-    onMounted(() => {
-        loadCharacters()
+const decrementePage = () => {
+    if(page.value > 1){
+    page.value--
+    loadCharacters()
+        }
+    }
+
+onMounted(() => {
+    loadCharacters()
     })
 </script>
 
