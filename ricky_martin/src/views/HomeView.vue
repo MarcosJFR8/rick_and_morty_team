162b918545e20fa753176be6930101e469658588
Marcos <marcosjfr3@gmail.com>
2025-03-08 16:53:41 -0400

    api fetch

--- ricky_martin/src/views/HomeView.vue
+++ ricky_martin/src/views/HomeView.vue
@@ -1,5 +1,29 @@
 <script setup lang="ts">
+import { ref, onMounted, } from "vue";
+import { useRouter } from "vue-router";
+    
+const router = useRouter();
+const characters = ref<Character[]>([])
 
+
+    const findCharacters = () => {
+        loadCharacters()
+    };
+
+    const loadCharacters = async () => {
+        const response = await fetch(`https://rickandmortyapi.com/api/character/`)
+        const data = await response.json()
+        characters.value = data.results
+        console.log();
+    };
+
+    const seeCharacterDetails =(character_id: number)=>{
+        router.push(`details/${character_id}`)
+    };   
+
+    onMounted(() => {
+        loadCharacters()
+    })
 </script>
 
 <template>
