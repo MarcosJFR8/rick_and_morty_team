ab1cec1f7ea54afd9b68365e4673ddbffe9ff088
unknown <santoyoisabella14@gmail.com>
2025-03-08 18:19:19 -0400

    boton para ver personajes

--- ricky_martin/src/views/HomeView.vue
+++ ricky_martin/src/views/HomeView.vue
@@ -48,14 +48,16 @@ const page:any = ref<number>(1)
     </h1>
     <div class="flex items-center space-x-4 justify-center text-center mt-10">
         <button @click="decrementePage" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">-</button>
-        {{ page }}
+        {{ page }} 
         <button @click="incrementPage" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full shadow-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">+</button>
     </div>
 
     <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-7 mt-24 mx-10">
         <div v-for="character in characters" :key="character.id">
-            <img :src="character.image" alt="character.name" />
-            <h2>{{ character.name }}</h2>
+            <div @click="seeCharacterDetails(character.id)">
+                <img :src="character.image" alt="character.name"/>
+                <h2>{{ character.name }}</h2>
+            </div>
         </div>
     </div>
 
