907e9051b6e4f93c2887377562565fcdb08d1e9e
Daniel <damm172004@gmail.com>
2025-03-15 16:30:15 -0400

    Tittle aesthetic

---
+++ src/views/AboutView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import type { Character } from '@/types/character';
+
+const route = useRoute()
+const id = route.params.id
+const character = ref<Character>()
+
+const loadCharacters = async () => {
+  const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+  const data = await response.json()
+  character.value = data
+}
+
+onMounted(() => {
+    loadCharacters()
+})
+</script>
+
+<template>
+    <div class="flex justify-center items-center h-screen">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:w-6xl" v-if="character">
+        <!-- Image Container -->
+        <div class="flex justify-center items-center">
+            <img class="rounded-2xl border-4 border-blue-800" :src="character.image" :alt="character.name">
+        </div>
+  
+        <!-- Description Card Container -->
+        <div class="flex justify-center items-center">
+        <div class="text-center p-4 shadow-2xl rounded-2xl bg-white">
+            <h1 class="text-2xl font-semibold mb-4">Nombre: {{ character.name }}</h1>
+            <p class="mb-2">Lugar: {{ character.location.name }}</p>
+            <p class="mb-2">Status: {{ character.status }}</p>
+            <p class="mb-2">Especie: {{ character.species }}</p>
+            <p class="mb-2">Genero: {{ character.gender }}</p>
+        </div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { ref, onMounted, } from "vue";
+import { useRouter } from "vue-router";
+import type { Character } from "@/types/character";
+    
+const router = useRouter();
+const characters = ref<Character[]>([])
+const page:any = ref<number>(1)
+
+
+const findCharacters = () => {
+    loadCharacters()
+    };
+
+const loadCharacters = async () => {
+    const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page.value}`)
+    const data = await response.json()
+    characters.value = data.results
+    console.log();
+    };
+
+const seeCharacterDetails =(character_id: number)=>{
+    router.push(`/about/${character_id}`)
+    };   
+
+const incrementPage = () => {
+    if(page.value !== null){
+        page.value++
+        loadCharacters()
+        }
+    }
+
+const decrementePage = () => {
+    if(page.value > 1){
+    page.value--
+    loadCharacters()
+        }
+    }
+
+onMounted(() => {
+    loadCharacters()
+    })
+</script>
+
+<template>
+    <h1 class="text-6xl md:text-8xl font-bold text-white text-center">
+        <span class="block mb-4">Bienvenido a</span>
+        <span class="block bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-blue-500">
+            Rick and Morty Team
+        </span>
+    </h1>
+    <div class="flex items-center space-x-4 justify-center text-center mt-10">
+        <button @click="decrementePage" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">-</button>
+        <div >
+            {{ page }} 
+        </div>
+        <button @click="incrementPage" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full shadow-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">+</button>
+    </div>
+
+    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-7 mt-24 mx-10">
+        <div v-for="character in characters" :key="character.id">
+            <div @click="seeCharacterDetails(character.id)">
+                <img :src="character.image" alt="character.name"/>
+                <h2>{{ character.name }}</h2>
+            </div>
+        </div>
+    </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
